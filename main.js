27c965021ff939e4c841a8c2164747e0a7bdcac5
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-11-11 23:31:36 +0600

    Coming Soon Upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+  strings: ["Wanna See??", "Are You Ready?", "Just Wait Some Days"],
+  typeSpeed: 20,
+  startDelay: 0,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true,
+  cursorChar: "|",
+  contentType: "html",
+});
