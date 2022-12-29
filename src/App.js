27f5a96be0815f81cf2c0ac8ea4344f57ca735b1
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-12-29 16:41:42 +0600

    axios delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Fragment, Component } from "react";
 import { Routes, Route } from "react-router-dom";
-import axios from "axios";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Homepage from "./home/Homepage";
@@ -12,17 +11,6 @@ export default class App extends Component {
   state = {
     data: DATA,
   };
-
-  // componentDidMount() {
-  //   axios
-  //     .get("https://webkit-8ac2e-default-rtdb.firebaseio.com/services.json")
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
 
   render() {
     // console.log(this.state.data.services);
