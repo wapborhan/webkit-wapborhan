571a5370784e7b68cf7972f37edcce414bec2557
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-10-07 22:54:59 +0600

    Change

--- src/pages/component/ComponentDetails.jsx
+++ src/pages/component/ComponentDetails.jsx
@@ -1,16 +1,7 @@
 import Input from "./input";
 
 const ComponentDetails = ({ pathName }) => {
-  const renderComponent = () => {
-    switch (pathName) {
-      case "input-text":
-        return <Input />;
-      default:
-        return <p>Component not found</p>;
-    }
-  };
-
-  return <div>{renderComponent()}</div>;
+  return <div>{pathName === "input-text" && <Input />}</div>;
 };
 
 export default ComponentDetails;
